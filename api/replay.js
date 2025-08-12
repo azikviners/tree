49ca5d8f5021981f5f1022d77823d46dbe302d7c
azikviners <azikviners@gmail.com>
2025-08-12 15:07:19 +0500

    Update replay.js

--- api/replay.js
+++ api/replay.js
@@ -2,7 +2,7 @@ import { getLastReply } from "./webhook";
 
 export const config = { runtime: "edge" };
 
-export default async function handler(req) {
+export default async function handler() {
   const reply = getLastReply();
   return new Response(JSON.stringify({ reply }), {
     headers: { "Content-Type": "application/json" },
